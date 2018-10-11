6b3a5c6ba6edc1326cc25eb4bdd8d77f1b69154a
ftarek <ftarek@softmills.com>
2018-10-11 09:31:27 +0200

    buyer web modify

--- resources/views/buyers/table.blade.php
+++ resources/views/buyers/table.blade.php
@@ -3,10 +3,10 @@
         <tr>
             <th>Name</th>
         <th>Email</th>
-        <th>Password</th>
+        <th>City</th>
         <th>Telephone</th>
         <th>Super Name</th>
-        <th>Token</th>
+        
             <th colspan="3">Action</th>
         </tr>
     </thead>
@@ -15,15 +15,15 @@
         <tr>
             <td>{!! $buyers->name !!}</td>
             <td>{!! $buyers->email !!}</td>
-            <td>{!! $buyers->password !!}</td>
+            <td>{!! $buyers->city !!}</td>
             <td>{!! $buyers->telephone !!}</td>
             <td>{!! $buyers->super_name !!}</td>
-            <td>{!! $buyers->token !!}</td>
+            
             <td>
                 {!! Form::open(['route' => ['buyers.destroy', $buyers->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('buyers.show', [$buyers->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('buyers.edit', [$buyers->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    <!-- <a href="{!! route('buyers.edit', [$buyers->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a> -->
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
                 {!! Form::close() !!}
