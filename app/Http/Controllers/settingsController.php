23690c164c4ec9f9e142a93bd1e2cdedd24366d0
ftarek <ftarek@softmills.com>
2018-10-11 10:54:27 +0200

    upload func

--- app/Http/Controllers/settingsController.php
+++ app/Http/Controllers/settingsController.php
@@ -26,6 +26,18 @@ class settingsController extends AppBaseController
 
     }
 
+ public   function uploadFile($field_name, $destination) {
+        if (!is_null(Input::file($field_name))) {
+            $file = Input::file($field_name)->getClientOriginalName();
+            $input[$field_name] = $file;
+            $file1 = Input::file($field_name);
+            $uploadSuccess = $file1->move($destination, $file);
+            return $file;
+        } else {
+            return false;
+        }
+    }
+
     /**
      * Display a listing of the settings.
      *
