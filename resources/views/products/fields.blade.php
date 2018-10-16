44c9cf66622d7f4e0e6d2fac97c045efc3ae66e8
ftarek <ftarek@softmills.com>
2018-10-16 16:10:31 +0200

    product modification

--- resources/views/products/fields.blade.php
+++ resources/views/products/fields.blade.php
@@ -39,13 +39,13 @@
 <!-- Discount Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('discount', 'Discount($):') !!}
-    {!! Form::text('discount', null, ['class' => 'form-control']) !!}
+    {!! Form::number('discount', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Discount Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('description', 'Description:') !!}
-    {!! Form::number('description', null, ['class' => 'form-control']) !!}
+    {!! Form::text('description', null, ['class' => 'form-control']) !!}
 </div> 
 <!-- Logo Field -->
 <div class="form-group col-sm-6">
